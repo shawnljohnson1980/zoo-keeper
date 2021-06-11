403673d3d727de2e077e9d203df5b01fd62e5442
Shawn Johnson <78113657+shawnljohnson1980@users.noreply.github.com>
2021-06-11 15:19:41 -0400

    Add files via upload

---
+++ module-info.java
@@ -0,0 +1,22 @@
+public class Zookeeper{
+    public static void main(String[] args){
+        
+        Gorilla newgor = new Gorilla();
+        newgor.throwSomething();
+        newgor.throwSomething();
+        newgor.throwSomething();
+        newgor.eatBananas();
+        newgor.eatBananas();
+        newgor.climb();
+
+        Bat newbat = new Bat();
+        newbat.attackTown();
+        newbat.attackTown();
+        newbat.attackTown();
+        newbat.eatHumans();
+        newbat.eatHumans();
+        newbat.fly();
+        newbat.fly();
+
+    }
+}
